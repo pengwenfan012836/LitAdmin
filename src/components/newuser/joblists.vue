d6372aaac7c9746bb343d7af3506c051e4fc3714
pwf <1240191963@qq.com>
2018-05-14 00:00:27 +0800

    添加走马灯

--- src/components/newuser/joblists.vue
+++ src/components/newuser/joblists.vue
@@ -16,10 +16,22 @@
 			<el-step ></el-step>
 		</el-steps>
 		<el-row class="content">
-			
+			<template>
+				<div class="block">
+				   <el-carousel height="600" :autoplay="autoplay">
+				     <el-carousel-item v-for="item in 4" :key="item">
+				       <h3>{{ item }}</h3>
+				     </el-carousel-item>
+				   </el-carousel>
+				 </div>
+			</template>
 		</el-row>
-		<el-button style="margin-top: 12px;" size="small" @click="next">下一步</el-button>
-		<el-button style="margin-top: 12px;" size="small" @click.native.prevent="goBack">取消</el-button>
+		<el-row  class="step-footer">
+			<el-button style="margin-top: 12px;" size="small" v-show="active!==0" @click="next(-1)">上一步</el-button>
+			<el-button style="margin-top: 12px;" size="small" v-show="active!==3" @click="next(1)">下一步</el-button>
+			<el-button style="margin-top: 12px;" size="small" v-show="active===3">完成</el-button>
+			<el-button style="margin-top: 12px;" size="small" @click.native.prevent="goBack">取消</el-button>
+		</el-row>
 	</div>
 </template>
 <script>
@@ -27,12 +39,23 @@
 	export default {
 	    data() {
 	      return {
-	        active: 0
+	        active: 0,
+	        autoplay: false
 	      };
 	    },
 	    methods: {
-	      next() {
-	        if (this.active++ > 2) this.active = 0;
+	      next(direction) {
+	      	if (direction > 0) {
+	      		this.active++;
+	      		if (this.active >= 3) {
+	      			this.active = 3;
+	      		}
+	      	} else {
+	      		this.active--;
+	      		if (this.active <= 0) {
+	      			this.active = 0;
+	      		}
+	      	}
 	      },
 	      goBack() {
 	      	history.back();
@@ -52,5 +75,24 @@
 		margin-right:3%;
 		margin:;
 		border: 1px solid #999;
+	}
+	.step-footer {
+		float: right;
+		margin-right: 3%;
+	}
+	.el-carousel__item h3 {
+		color: #475669;
+		font-size: 14px;
+		opacity: 0.75;
+		line-height: 150px;
+		margin: 0;
+	}
+
+	.el-carousel__item:nth-child(2n) {
+		background-color: #99a9bf;
+	}
+
+	.el-carousel__item:nth-child(2n+1) {
+		background-color: #d3dce6;
 	}
 </style>
